4b6888803815b5482407d7aa4244d063b9ebc541
RhoPaper <rzj44346@outlook.com>
2025-02-11 19:25:34 +0800

    更新 config.js

--- public/config.js
+++ public/config.js
@@ -5,12 +5,7 @@ window.Config = {
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
-  ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
-  ],
+  ApiKeys: ['m798559755-87a27de562c1bdd5b6e9e0ed'],
 
   // 日志天数
   CountDays: 90,
